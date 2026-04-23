03140f949f0e58f480c8ea79e8cde4a9d5b0f09c
sp11ker <71824282+sp11ker@users.noreply.github.com>
2026-04-23 18:06:35 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,21 @@
+###############################
+# Public IP of VM
+###############################
 output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
 }
 
+###############################
+# Private Key (sensitive)
+###############################
 output "private_key_pem" {
   value     = tls_private_key.example.private_key_pem
   sensitive = true
 }
 
-resource "local_file" "private_key" {
-  content  = tls_private_key.example.private_key_pem
-  filename = "${path.module}/my-keypair.pem"
-  file_permission = "0400"
+###############################
+# SSH Command (bonus)
+###############################
+output "ssh_command" {
+  value = "ssh -i my-keypair.pem debian@${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
 }
